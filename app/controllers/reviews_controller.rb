cb4b09ffcc17e4fc7f9e9967c35e2b04389ce4cc
Michael Lee <mike.epiphany@gmail.com>
2016-02-29 14:46:12 -0800

    AJAXIFY delete, and showed errors

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,17 +15,23 @@ class ReviewsController < ApplicationController
     review_params = params.require(:review).permit(:body)
     @review = Review.new(review_params)
     @review.product = @product
+    respond_to do |format|
     if @review.save
-      redirect_to product_path(@product), notice: "Review created!"
+      format.html { redirect_to product_path(@product), notice: "Review created!" }
+      format.js { render :create_success }
     else
-    render "/products/show"
+      format.html{ render "/products/show" }
+      format.js { render :create_failure }
     end
+   end
   end
 
   def destroy
-    review = Review.find params[:id]
-    review.destroy
-    redirect_to product_path(params[:product_id]), notice: "Review deleted"
+    @review = Review.find params[:id]
+    @review.destroy
+    respond_to do |format|
+    format.html {redirect_to product_path(params[:product_id]), notice: "Review deleted"}
+    format.js { render }
   end
-
+ end
 end
